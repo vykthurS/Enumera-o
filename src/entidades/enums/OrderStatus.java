fc3a138aaa39c156f94eed5ffde8641e4eaafafa
vykthurS <vykthurrr@gmail.com>
2020-02-22 22:44:31 -0300

    utilização de enum

---
+++ src/entidades/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package entidades.enums;
+
+public enum OrderStatus {
+	aguardando_pagamento,
+	processando,
+	enviado,
+	entregue,
+
+}
--- src/programa/Aplicação.java
+++ src/programa/Aplicação.java
@@ -1,9 +1,23 @@
 package programa;
 
+import java.util.Date;
+
+import entidades.Pedido;
+import entidades.enums.OrderStatus;
+
 public class Aplicação {
 
 	public static void main(String[] args) {
 		
+		Pedido pedido= new Pedido (1080, new Date(), OrderStatus.aguardando_pagamento);
+		
+		System.out.println(pedido);
+		
+		OrderStatus os1= OrderStatus.entregue;
+		OrderStatus os2 = OrderStatus.valueOf("entregue");
+		
+		System.out.println(os1);
+		System.out.println(os2);
 
 	}
 
